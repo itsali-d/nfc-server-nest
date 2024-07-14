18b4913cc6bec12536da63800e5f2c8a7382f197
itsalimanuel <itskhaloufali@gmail.com>
2024-07-14 15:54:14 +0400

    (chore): default

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { DefaultSeed } from 'src/seeder/default.seeder';
+import { OrgUser, OrgUserSchema } from 'src/utils';
 
 @Module({
   imports: [
     OrgUserModule,
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MDB_URL),
+    MongooseModule.forFeature([{ name: OrgUser.name, schema: OrgUserSchema }]),
     UserModule,
   ],
   controllers: [AppController],
