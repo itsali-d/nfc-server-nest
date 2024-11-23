b16621a3d60aae765f147e709a641f04833b0228
itsali-d <cryptoraidar@gmail.com>
2024-11-23 15:05:58 +0400

    chore: offer

--- src/offer/offer.controller.ts
+++ src/offer/offer.controller.ts
@@ -1,50 +1,49 @@
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    Patch,
-    Post,
-    UseGuards,
-  } from '@nestjs/common';
-  import { AuthGuard } from '@nestjs/passport';
-  import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-  import { CreateOfferDto, DeleteOfferDto, UpdateOfferDto } from 'src/utils';
-  import { OfferService } from './offer.service';
-  @ApiTags('Offer')
-  @Controller('offer')
-  export class OfferController {
-    constructor(private readonly offerService: OfferService) {}
-    @Post()
-    @UseGuards(AuthGuard('user'))
-    @ApiBearerAuth()
-    create(@Body() createOfferDto: CreateOfferDto) {
-      return this.offerService.create(createOfferDto);
-    }
-  
-  //   @Get()
-  //   findAll() {
-  //     return this.offerService.findAll();
-  //   }
-  
-  //   @Get(':id')
-  //   findOne(@Param('id') id: string) {
-  //     return this.offerService.findOne(id);
-  //   }
-  
-    @Patch('')
-    @UseGuards(AuthGuard('user'))
-    @ApiBearerAuth()
-    update(@Body() updateOfferDto: UpdateOfferDto) {
-      return this.offerService.update(updateOfferDto);
-    }
-  
-    @Delete('')
-    @UseGuards(AuthGuard('user'))
-    @ApiBearerAuth()
-    remove(@Body() deleteOfferDto: DeleteOfferDto) {
-      return this.offerService.remove(deleteOfferDto);
-    }
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CreateOfferDto, DeleteOfferDto, UpdateOfferDto } from 'src/utils';
+import { OfferService } from './offer.service';
+@ApiTags('Offer')
+@Controller('offer')
+export class OfferController {
+  constructor(private readonly offerService: OfferService) {}
+  @Post()
+  @UseGuards(AuthGuard('user'))
+  @ApiBearerAuth()
+  create(@Body() createOfferDto: CreateOfferDto) {
+    return this.offerService.create(createOfferDto);
   }
-  
+
+  @Get()
+  findAll() {
+    return this.offerService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.offerService.findOne(id);
+  }
+
+  @Patch('')
+  @UseGuards(AuthGuard('user'))
+  @ApiBearerAuth()
+  update(@Body() updateOfferDto: UpdateOfferDto) {
+    return this.offerService.update(updateOfferDto);
+  }
+
+  @Delete('')
+  @UseGuards(AuthGuard('user'))
+  @ApiBearerAuth()
+  remove(@Body() deleteOfferDto: DeleteOfferDto) {
+    return this.offerService.remove(deleteOfferDto);
+  }
+}
